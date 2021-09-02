c60693c86967840a4552ec4fe8c3480ddb7e0939
Aniket Sarkar <aniketsarkar1998@gmail.com>
2021-09-02 15:54:47 +0530

    rename the typing objects for url-patterns

--- navycut/typing/urls.py
+++ navycut/typing/urls.py
@@ -3,6 +3,6 @@ import typing as t
 if t.TYPE_CHECKING:
     from navycut import urls
 
-urlPatterns = t.List[t.Union["urls.url", "urls.path", "urls.include"]]
+ncUrlPatterns = t.List[t.Union["urls.url", "urls.path", "urls.include"]]
 
-urlPattern = t.Tuple[t.List[t.Union["urls.url", "urls.path", "urls.include"]]]
+ncUrlPattern = t.Tuple[t.List[t.Union["urls.url", "urls.path", "urls.include"]]]
